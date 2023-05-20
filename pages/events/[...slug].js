015e2c4213dc95db8ab92d21650f0bce428902ef
Prajwal Mahajan <82263793+prajwalmahajan101@users.noreply.github.com>
2023-05-20 09:06:32 +0530

    routes defined and Dummy data added

---
+++ pages/events/[...slug].js
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+
+const FilteredEventsPage = () => {
+	return (
+		<div>
+			<h1>The Filtered Event Page</h1>
+		</div>
+	);
+};
+export default FilteredEventsPage;
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const HomePage = () => {
+	return (
+		<div>
+			<h1>The Home Page</h1>
+		</div>
+	);
+};
+export default HomePage;
