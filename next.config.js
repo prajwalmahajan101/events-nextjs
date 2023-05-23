1bb5d6acc618072446ba7fce186ed6f971cd62c6
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2023-05-23 14:58:30 +0530

    Mongodb Connected to the App

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const env =  require('./env');
 const nextConfig = {
   reactStrictMode: true,
+  env:{
+    ...env
+  }
 }
 
 module.exports = nextConfig
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,15 +1,23 @@
-const handler = async (req, res) => {
-	const { method } = req;
-	if (method === "POST") {
-		const { email } = req.body;
-		if (!email || !email.includes("@")) {
-			return res.status(422).json({
-				message: "Invalid Email address",
-			});
-		}
+import { MongoClient } from "mongodb";
 
-		console.log(email);
-		return res.status(201).json({ email });
-	}
+const handler = async (req, res) => {
+  const { method } = req;
+  if (method === "POST") {
+    const { email } = req.body;
+    if (!email || !email.includes("@")) {
+      return res.status(422).json({
+        message: "Invalid Email address",
+      });
+    }
+    try {
+      const client = await MongoClient.connect(process.env.DB_URL);
+      const db = client.db();
+      await db.collection("emails").insertOne({ email });
+	  client.close();
+    } catch (err) {
+      console.log(err);
+    }
+    return res.status(201).json({ email });
+  }
 };
 export default handler;
